670b359d61ea2fb358ee86eb2c4a76dea5cc326f
WalanyCosta <walanybnegro@gmail.com>
2024-04-13 03:41:24 +0100

    adicionar docker e express

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
-function imprimir(mensagem: string): void {
-    console.log(mensagem)
-}
+import express, { type Request, type Response } from 'express'
 
-imprimir('Olá mundo!')
+const port = process.env.PORT
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  return res.json({ mensagem: 'Olá, mundo!!' })
+})
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})

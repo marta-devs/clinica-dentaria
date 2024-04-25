9fe7f5d4de180599b520b16a4864e2882d5c4a4e
WalanyCosta <walanybnegro@gmail.com>
2024-04-25 10:33:04 +0100

    adicionar cors no project

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import loginRoutes from './routes/login-routes'
 import dentistaRoutes from './routes/dentista-routes'
 import tiposConsultaRoutes from './routes/tipo-consulta-routes'
 import consultaRoutes from './routes/consulta-routes'
+import cors from 'cors'
 
 const port = process.env.PORT
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+
 app.use(loginRoutes)
 app.use(dentistaRoutes)
 app.use(tiposConsultaRoutes)

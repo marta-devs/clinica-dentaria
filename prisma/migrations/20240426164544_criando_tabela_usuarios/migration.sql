3863523076ba82b898e1ba034ddc0bcb7d6f7bb2
WalanyCosta <walanybnegro@gmail.com>
2024-04-27 10:44:14 +0100

    resolvido o error está acontecendo no prisma

--- prisma/migrations/20240426164544_criando_tabela_usuarios/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `usuario` DROP FOREIGN KEY `Usuario_pacienteId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `usuario` ADD CONSTRAINT `usuario_pacienteId_fkey` FOREIGN KEY (`pacienteId`) REFERENCES `Paciente`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
